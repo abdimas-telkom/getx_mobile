ac99656d313d350c2f7850a2f35757ae13b9e0b4
Alvan <alvanmarvianto@gmail.com>
2025-07-01 19:00:53 +0700

    update for deploy

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -3,8 +3,11 @@ import 'package:dio/dio.dart';
 class ApiClient {
   static final Dio _dio = Dio(
     BaseOptions(
-      baseUrl: 'http://10.0.2.2:8000/api',
+      baseUrl: 'http://40.65.153.112:8080/api',
       headers: {'Content-Type': 'application/json'},
+      validateStatus: (status) {
+        return status != null && status < 500;
+      },
     ),
   );
 
